25f4979e2f80e4aa85c5194d8a187b73ad573e23
michal <michal@lab42.pl>
2016-09-21 01:03:12 +0200

    initial package

---
+++ spec/git-checkout-control-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GitCheckoutControlView from '../lib/git-checkout-control-view';
+
+describe('GitCheckoutControlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
